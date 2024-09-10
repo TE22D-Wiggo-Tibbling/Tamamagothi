a04c58df841f6e756dcff8261b5043c67e42756e
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2024-09-10 11:14:02 +0200

    amamoshi-su

--- Tamagoshuis/Tamamagoshi.cs
+++ Tamagoshuis/Tamamagoshi.cs
@@ -1,4 +1,39 @@
 public class Tamamagoshi
 {
+private int hunger;
+private int bredom;
+private List<string> words;
+private Random generator;
+public string Name;
+private bool isAlive;
 
+public void Feed()
+{
+
+}
+
+public void Hi()
+{
+
+}
+
+public void Tick()
+{
+
+}
+
+public void PrintStats()
+{
+
+}
+
+public bool GetAlive()
+{
+    return isAlive;
+}
+
+private void ReduceBoredom()
+{
+
+}
 }
