4dce0e903ab341ff345ebb7ade7d9fc03b5eac23
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2024-10-01 10:34:36 +0200

    tammmammagishi

--- Tamagoshuis/Program.cs
+++ Tamagoshuis/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Tamamagoshi tamamagoshi = new();
+
+Console.WriteLine("naeme tamamagoishi");
+
+tamamagoshi.Name = Console.ReadLine();
+while(tamamagoshi.GetAlive()){
+
+System.Console.WriteLine("What you wana do?");
+System.Console.WriteLine("1) lär nytt ord   2) säga hejhej   3) mata   4) skit i");
+
+if (Console.ReadLine() == "1")
+{
+    System.Console.WriteLine("WHAT YOU LERN IT?");
+    string wordToLearn = Console.ReadLine();
+    tamamagoshi.Teach(wordToLearn);
+}
+else if(Console.ReadLine() == "2") tamamagoshi.Hi();
+}
+
+Console.ReadLine();
--- Tamagoshuis/Tamamagoshi.cs
+++ Tamagoshuis/Tamamagoshi.cs
@@ -1,39 +1,49 @@
 public class Tamamagoshi
 {
-private int hunger;
-private int bredom;
-private List<string> words;
-private Random generator;
-public string Name;
-private bool isAlive;
+    private int hunger;
+    private int bredom;
+    private List<string> words = new();
+    private Random generator;
+    public string Name;
+    private bool isAlive;
 
-public void Feed()
-{
+    public void Feed()
+    {
+        hunger -= 10;
+    }
 
-}
+    public void Hi()
+    {
+        System.Console.WriteLine(words[generator.Next(words.Count)]);
+    }
 
-public void Hi()
-{
+    public void Tick()
+    {
+        hunger++;
+        bredom++;
+        if (hunger > 10 || bredom > 10)
+        {
+            isAlive = false;
+        }
+    }
 
-}
+    public void PrintStats()
+    {
+        System.Console.WriteLine($"hunger: {hunger} boredome: {bredom}");
+    }
 
-public void Tick()
-{
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
 
-}
+    private void ReduceBoredom()
+    {
+        bredom -= 2;
+    }
 
-public void PrintStats()
-{
-
-}
-
-public bool GetAlive()
-{
-    return isAlive;
-}
-
-private void ReduceBoredom()
-{
-
-}
+    public void Teach(string word)
+    {
+        words.Add(word);
+    }
 }
